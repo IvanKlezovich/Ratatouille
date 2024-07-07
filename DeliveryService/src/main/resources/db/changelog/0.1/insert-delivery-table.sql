8c3b6c6f2eb6068d6f60c4e7e4c32228b2362cf4
Niki1os1 <elcrustallshull11@gmail.com>
2024-07-07 15:34:53 +0300

    add delivery-service

---
+++ DeliveryService/src/main/resources/db/changelog/0.1/insert-delivery-table.sql
@@ -0,0 +1,4 @@
+INSERT INTO delivery (order_id, status, message)
+VALUES ('1', 'DELIVERED', 'Order delivered successfully.'),
+       ('2', 'NOT_DELIVERED', 'Cannot deliver at the moment.'),
+       ('3', 'DELIVERED', 'Order delivered successfully.');
